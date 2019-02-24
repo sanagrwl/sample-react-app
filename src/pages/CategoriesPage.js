2b4555f54687f13f71ff0b586b2b37e664ccc93e
Sandeep Aggarwal <1342667+sanagrwl@users.noreply.github.com>
2019-02-24 13:47:38 -0600

    updated import path for categories component to default directory export

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Categories from '../components/categories/CategoriesContainer'
+import Categories from '../components/categories'
 
 const CategoriesPage = () => {
     return (
